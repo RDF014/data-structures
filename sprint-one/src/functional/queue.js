079096b4878431ed5f1955f7c98791554351a5de
rdf014 <rdf014@gmail.com>
2017-02-08 16:34:12 -0800

    "Stack and Queue the functional pattern"

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,26 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
+  
+  var addCounter = 0;
+  var removeCounter = 0;
   // Implement the methods below
-
   someInstance.enqueue = function(value) {
+    storage[addCounter] = value;
+    size++;
+    addCounter++;
   };
 
   someInstance.dequeue = function() {
+    size--;  
+    var item = storage[removeCounter];
+    removeCounter++;
+    return item;
   };
 
   someInstance.size = function() {
+    return Math.max(0, size);
   };
 
   return someInstance;

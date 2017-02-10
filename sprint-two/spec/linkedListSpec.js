335d3ede9e750b673c62e56743046c0cac7f8283
rdf014 <rdf014@gmail.com>
2017-02-09 16:10:03 -0800

    LinkedListSpec completed

--- sprint-two/spec/linkedListSpec.js
+++ sprint-two/spec/linkedListSpec.js
@@ -24,11 +24,13 @@ describe('linkedList', function() {
   });
 
   it('should remove the head from the list when removeHead is called', function() {
+    console.log("-------------------------------------------");
     linkedList.addToTail(4);
     linkedList.addToTail(5);
     expect(linkedList.head.value).to.equal(4);
     linkedList.removeHead();
     expect(linkedList.head.value).to.equal(5);
+    console.log("-------------------------------------------");
   });
 
   it('should return the value of the former head when removeHead is called', function() {
--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,30 @@
 var LinkedList = function() {
   var list = {};
+
   list.head = null;
   list.tail = null;
 
+  
   list.addToTail = function(value) {
+    list.tail = Node(value);
+    if (list.head === null) {
+      list.head = Node(value);
+    } else {
+      list.head.next = list.tail.value;
+    }
   };
 
   list.removeHead = function() {
+    var headToRemove = list.head.value;
+    list.head.value = list.tail.value;
+    return headToRemove;
   };
 
   list.contains = function(target) {
+    if (list.head.value === target || list.tail.value === target) {
+      return true;
+    } 
+    return false;
   };
 
   return list;
@@ -26,4 +41,10 @@ var Node = function(value) {
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  // addToTail will have O(1) constant complexity as we create a new node,
+  change current tail's reference to point to the new node, and change the new 
+  tail to refer to last value
+  // removeHead will have O(1) constant complexity as we remove the head, and just reference to
+  //the next item in the list
+  //contains will have O(1) constant complexity as we choose either head or tail for the target value
  */
